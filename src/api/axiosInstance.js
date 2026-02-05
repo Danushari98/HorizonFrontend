bb348a37dd7ede1e92cf6a9b048de16f05d29dc0
Danushari98 <danushari98@gmail.com>
2026-02-05 09:23:59 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://travel-backend-xurz.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL || "https://travel-backend-xurz.onrender.com/api",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
